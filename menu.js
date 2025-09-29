f137a99213554a29b0612f83f9e0efcd4cb3ad42
Yellychan16 <chrisguinto669@gmail.com>
2025-09-29 09:53:14 +0800

    Initial commit

--- menu.js
+++ menu.js
@@ -8,18 +8,4 @@ if (username) {
   console.log("Saved username in localStorage:", username);
 }
 
-// Display welcome message
-const welcomeMsg = document.getElementById("welcome-msg");
 
-if (fullname) {
-  welcomeMsg.textContent = `Welcome, ${fullname}!`;
-} else {
-  // If no username in URL, try localStorage
-  const storedUser = localStorage.getItem("username");
-  if (storedUser) {
-    welcomeMsg.textContent = `Welcome back, ${storedUser}!`;
-  } else {
-    // Not logged in → redirect to login page
-    window.location.href = "index.html";
-  }
-}
